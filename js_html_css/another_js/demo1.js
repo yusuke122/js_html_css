030582ba42fc4f134de475030bacca54c5ffc041
佑輔 伏木 <youfufumu@gmail.com>
2022-04-05 05:24:57 +0900

    window object test

--- js_html_css/another_js/demo1.js
+++ js_html_css/another_js/demo1.js
@@ -2,6 +2,14 @@ const demo1function = () => {
     alert("demo1 function");
 }
 
+var vartime = 100;
+let lettime = 1000;
+const consttime = 10000;
+
 // windowオブジェクトのグローバル変数用キーに定義した関数を入れる
 window.globalFunction = {};
 window.globalFunction.demo1 = demo1function;
+window.globalElem = {};
+window.globalElem.varElem = vartime;
+window.globalElem.letElem = lettime;
+window.globalElem.constElem = consttime;
--- js_html_css/another_js/demo2.js
+++ js_html_css/another_js/demo2.js
@@ -1,1 +1,6 @@
 document.getElementById("button").addEventListener('click', () => { window.globalFunction.demo1() });
+document.getElementById("elembutton").addEventListener('click', () => {
+    alert(window.globalElem.varElem);
+    alert(window.globalElem.letElem);
+    alert(window.globalElem.constElem);
+});
